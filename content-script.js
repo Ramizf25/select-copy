7eddbf982e31913426909aff06c341428f74154a
Ramizf25 <ramiz9119@gmail.com>
2023-01-17 00:17:43 +0530

    save button added

--- content-script.js
+++ content-script.js
@@ -1,17 +1,29 @@
 /*
 copy the selected text to clipboard
 */
+let nextId = 0;
+data = [{ id: "", value: ""}];
 function copySelection() {
-    let selectedText = window.getSelection().toString().trim();
-    console.log(selectedText)
+  
+  
+  let selectedText = window.getSelection().toString().trim();
+  console.log(data);
 
-    if (selectedText) {
-        navigator.clipboard.writeText(selectedText);
-        console.log(navigator.clipboard.writeText(selectedText))
-    }
+  if (selectedText) {
+    navigator.clipboard.writeText(selectedText);
+
+    data.push( { id: nextId++, value: selectedText });
+  }
 }
+
+function listenForClicks() {
+    alert ("Hello World!");
+       };
+  
 
 /*
 Add copySelection() as a listener to mouseup events.
 */
-document.addEventListener("mouseup", copySelection);
+document.addEventListener("mouseup", copySelection)
+// .then(listenForClicks)
+document.getElementById("myBtn").addEventListener("click", listenForClicks());
