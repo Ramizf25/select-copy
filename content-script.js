4457ccbf98986559d5bcd82151351cf4fc54f332
Ramizf25 <ramiz9119@gmail.com>
2023-01-17 23:48:38 +0530

    copy to console applied with cntl+c

--- content-script.js
+++ content-script.js
@@ -4,21 +4,22 @@ let nextId = 0;
 data = [];
 
 function copySelection() {
-  let selectedText = window.getSelection().toString().trim();
-  console.log(data);
-
-  if (selectedText) {
-    navigator.clipboard.writeText(selectedText);
-
-    data.push({ id: nextId++, value: selectedText });
+  // let selectedText = window.getSelection().toString().trim();
+  let startupName = document.getElementsByClassName("styles_motionContainer__0bu1f")[0]
+  .getElementsByClassName("styles_name__zvQcy")[0].textContent;
+  let tagline = document.getElementsByClassName("styles_motionContainer__0bu1f")[0]
+  .getElementsByClassName("styles_subheader__SIX10")[0].textContent; 
+  let website = document.getElementsByClassName("styles_websiteLink___Rnfc")[0].textContent;
+  let location = document.getElementsByClassName("styles-module_component__vnXTJ m-0 text-sm")[0].textContent; 
+  let founder = document.getElementsByClassName("styles_identity__CgS8E")[0].getElementsByClassName("styles_component__UCLp3 styles_defaultLink__eZMqw")[0].innerHTML 
+  let fundingValuation = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[0].textContent 
+  
+  console.log(startupName,tagline,website,location,founder,fundingValuation);
+  if (tagline) {
+    data.push(startupName,tagline,website,location,founder,fundingValuation);
+    navigator.clipboard.writeText(JSON.stringify(data));
   }
 }
 
-function listenForClicks() {
-  alert("Hello World!");
-  // const info = loadJSON("results.json");
-  // data.forEach((item) => info.information.push(item));
-  // saveJSON('results.json',info)
-}
 
-document.addEventListener("mouseup", copySelection);
+document.addEventListener("copy", copySelection);
---
+++ popup/popup.js
@@ -0,0 +1,67 @@
+function functionName(){
+	alert('Hello world');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const hidePage = `body > :not(.beastify-image) {
+//     display: none;
+//   }`;
+
+
+// function listenForClicks() {
+// document.addEventListener("click", (e) => {
+
+// /**
+// * Given the name of a beast, get the URL to the corresponding image.
+// */
+// function beastNameToURL(beastName) {
+// switch (beastName) {
+// case "Save":
+// alert("hello world");
+
+// }
+// }
+
+// function beastify(tabs) {
+// browser.tabs.insertCSS({code: hidePage}).then(() => {
+// let url = beastNameToURL(e.target.textContent);
+// browser.tabs.sendMessage(tabs[0].id, {
+// command: "beastify",
+// beastURL: url
+// });
+// });
+// }
+
+
+// if (e.target.type === "reset") {
+// browser.tabs.query({active: true, currentWindow: true})
+// .then(reset)
+// .catch(reportError);
+// } else {
+// browser.tabs.query({active: true, currentWindow: true})
+// .then(beastify)
+// .catch(reportError);
+// }
+// });
+// }
+
+// browser.tabs.executeScript({file: "/beastify.js"})
+// .then(listenForClicks)
