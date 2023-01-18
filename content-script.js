e0476263b494ff21a79927d350f5a93247442376
Ramizf25 <ramiz9119@gmail.com>
2023-01-18 22:10:51 +0530

    all required fields scraped

--- content-script.js
+++ content-script.js
@@ -1,22 +1,30 @@
-// import { saveJSON, loadJSON } from "./readwritejson";
-
 let nextId = 0;
 data = [];
 
 function copySelection() {
   // let selectedText = window.getSelection().toString().trim();
+  let domain = document.getElementsByClassName("styles_component__NjqHY")[0].baseURI;
   let startupName = document.getElementsByClassName("styles_motionContainer__0bu1f")[0]
   .getElementsByClassName("styles_name__zvQcy")[0].textContent;
   let tagline = document.getElementsByClassName("styles_motionContainer__0bu1f")[0]
   .getElementsByClassName("styles_subheader__SIX10")[0].textContent; 
-  let website = document.getElementsByClassName("styles_websiteLink___Rnfc")[0].textContent;
-  let location = document.getElementsByClassName("styles-module_component__vnXTJ m-0 text-sm")[0].textContent; 
-  let founder = document.getElementsByClassName("styles_identity__CgS8E")[0].getElementsByClassName("styles_component__UCLp3 styles_defaultLink__eZMqw")[0].innerHTML 
-  let fundingValuation = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[0].textContent 
+  let website = document.getElementsByClassName("styles_websiteLink___Rnfc")[0].innerHTML;
+  // let linkdin = document.getElementsByClassName("styles_component__g_WAp")[0].getElementsByTagName("li")[5].innerHTML;
+  let location = document.getElementsByClassName("styles_metadata__NlXW0")[0].getElementsByTagName("dt")[1].innerText;
+  let companySize = document.getElementsByClassName("styles_metadata__NlXW0")[0].getElementsByTagName("dt")[3].innerText;
+  let markets = document.getElementsByClassName("styles_markets__siYMy")[0].innerText;
+	// let fundingDetailed = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].innerText;
+	// let fundingValuation = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[0].textContent; 
+	// let fundingRoundCount = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[1].textContent;
+	// let fundingLatestRound = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[2].textContent; 
+	let founder = document.getElementsByClassName("styles_component__ivX7J styles_twoColumn__XlBrn")[0].innerText ;
   
-  console.log(startupName,tagline,website,location,founder,fundingValuation);
-  if (tagline) {
-    data.push(startupName,tagline,website,location,founder,fundingValuation);
+  console.log(startupName,tagline,website,location,companySize,markets,founder)
+  
+  // ,website,location,companySize,markets,founder,
+  //   fundingValuation,fundingRoundCount,fundingLatestRound);
+  if (domain === "https://angel.co/jobs") {
+    data.push(startupName,tagline,website,location,companySize,markets,founder);
     navigator.clipboard.writeText(JSON.stringify(data));
   }
 }
--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,4 @@
-function functionName(){
+function alertFunction(){
 	alert('Hello world');
 }
 
@@ -7,9 +7,37 @@ function functionName(){
 
 
 
+// function copySelection() {
+  
+// 	// let selectedText = window.getSelection().toString().trim();
+// 	let domain = document.getElementsByClassName("styles_component__NjqHY")[0].baseURI;
+// 	let startupName = document.getElementsByClassName("styles_motionContainer__0bu1f")[0]
+// 	.getElementsByClassName("styles_name__zvQcy")[0].textContent;
+// 	let tagline = document.getElementsByClassName("styles_motionContainer__0bu1f")[0]
+// 	.getElementsByClassName("styles_subheader__SIX10")[0].textContent; 
+// 	let website = document.getElementsByClassName("styles_websiteLink___Rnfc")[0].innerHTML;
+// 	let linkdin = document.getElementsByClassName("styles_component__g_WAp")[0].getElementsByTagName("li")[5].innerHTML;
+// 	let location = document.getElementsByClassName("styles_metadata__NlXW0")[0].getElementsByTagName("dt")[1].innerText;  
+// 	let companySize = document.getElementsByClassName("styles_metadata__NlXW0")[0].getElementsByTagName("dt")[3].innerText;
+// 	let locationDetailed = document.getElementsByClassName("styles_metadata__NlXW0")[0].innerText;
+// 	let markets = document.getElementsByClassName("styles_markets__siYMy")[0].innerText;
+// 	let fundingDetailed = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].innerText;
+// 	let fundingValuation = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[0].textContent; 
+// 	let fundingRoundCount = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[1].textContent;
+// 	let fundingLatestRound = document.getElementsByClassName("styles_component__ivX7J styles_threeColumn__Txyiv")[0].getElementsByClassName("styles-module_component__3ZI84 styles_value__Bfjke text-lg font-medium")[2].textContent; 
+// 	let founder = document.getElementsByClassName("styles_component__ivX7J styles_twoColumn__XlBrn")[0].innerText ;
+	
+// 	console.log(domain,startupName,tagline,website,linkdin,location,companySize,markets,
+// 	  fundingValuation,fundingRoundCount,fundingLatestRound,founder);
 
 
-
+// 	  if (domain === "https://angel.co/jobs") {
+// 		data.push(domain,startupName,tagline,website,location,founder,fundingValuation);
+// 		navigator.clipboard.writeText(JSON.stringify(data));
+// 	  }
+// 	  else{
+// 		alert("This page is not supported")
+// 	  }
 
 
 
